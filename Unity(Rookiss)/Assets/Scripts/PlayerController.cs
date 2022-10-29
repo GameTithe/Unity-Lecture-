e8664a7f452432b3b64a1f6f2fba6a3a0c6c50a4
jangjaebum <wkdwobum@gmail.com>
2022-10-30 00:01:21 +0900

    Player Movement(+ Rotation)

--- Unity(Rookiss)/Assets/Scripts/PlayerController.cs
+++ Unity(Rookiss)/Assets/Scripts/PlayerController.cs
@@ -12,24 +12,42 @@ public class PlayerController : MonoBehaviour
         
     }
 
+    float _yAngle = 0.0f;
     void Update()
     {
-        // Local -> World
-        // transform.TransformDirection
+        _yAngle += Time.deltaTime * _speed;
 
-        // World -> Local
-        // InverseTrasnsformDirection
+        //+= delta
+        //transform.Rotate(new Vector3(0.0f, Time.deltaTime * 100.0f, 0.0f));
+
+        //transform.rotation = Quaternion.Euler(new Vector3(0.0f, _yAngle, 0.0f));
+
+
+        //transform.rotation
 
         if (Input.GetKey(KeyCode.W))
-            transform.Translate(Vector3.forward * Time.deltaTime * _speed);
-
+        {
+            transform.rotation = Quaternion.Slerp(transform.rotation, Quaternion.LookRotation(Vector3.forward), 0.2f);
+            transform.position += Vector3.forward * Time.deltaTime * _speed;
+        }
+        
         if (Input.GetKey(KeyCode.S))
-            transform.Translate(Vector3.back * Time.deltaTime * _speed);
+        {
+            transform.rotation = Quaternion.Slerp(transform.rotation, Quaternion.LookRotation(Vector3.back), 0.2f);
+            transform.position += Vector3.back * Time.deltaTime * _speed;
+
+        }
 
         if (Input.GetKey(KeyCode.A))
-            transform.Translate(Vector3.left * Time.deltaTime * _speed);
+        {
+            transform.rotation = Quaternion.Slerp(transform.rotation, Quaternion.LookRotation(Vector3.left), 0.2f);
+            transform.position += Vector3.left * Time.deltaTime * _speed;
+        }
 
         if (Input.GetKey(KeyCode.D))
-            transform.Translate(Vector3.right * Time.deltaTime * _speed);
+        {
+            transform.rotation = Quaternion.Slerp(transform.rotation, Quaternion.LookRotation(Vector3.right), 0.2f);
+            transform.position += Vector3.right * Time.deltaTime * _speed;
+        }
     }
 }
